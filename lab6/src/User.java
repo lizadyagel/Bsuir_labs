9774d36ed9fd81bda7a785a48441e25abf056e4f
lizadyagel <lizadagel@gmail.com>
2020-12-15 04:11:51 +0300

    Initial commit

---
+++ lab6/src/User.java
@@ -0,0 +1,14 @@
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+
+    public class User {
+        public static void main(String[] args) {
+            Reader med = new Reader("Электронная библиотека");
+            med.setVisible(true);
+            med.setResizable(false);
+            med.setLocationRelativeTo(null);
+        }
+    }
+
+
